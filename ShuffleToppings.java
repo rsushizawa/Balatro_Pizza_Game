48db1b2b0ed13ebd1a217bb8508e7258f97600dd
Daniel Wu <danielwu288@gmail.com>
2025-09-24 17:30:47 -0300

    random topping method

---
+++ ShuffleToppings.java
@@ -0,0 +1,16 @@
+package Balatro_Pizza_Game;
+
+import java.util.*;
+public class ShuffleToppings {
+    public static void main(String[] args) {
+        List<Toppings> pizzaToppings = new ArrayList<>(Arrays.asList(Toppings.values()));
+        Set<Toppings> pizzaOrder= EnumSet.noneOf(Toppings.class);
+        Random rand = new Random();
+        int i;
+        Collections.shuffle(pizzaToppings);
+        for ( i= 0; i < 6; i ++) {
+            pizzaOrder.add(pizzaToppings.get(i));
+        }
+        System.out.println(pizzaOrder);
+    }
+}
